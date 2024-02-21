6e92bd35064ef125728d589c1110748d33438b33
Antonio Santana <antonionetosg6@gmail.com>
2024-02-20 21:00:36 -0300

    feat: adiciona corsConfiguration

--- src/test/java/com/y/back/YBackEndApplicationTests.java
+++ src/test/java/com/y/back/YBackEndApplicationTests.java
@@ -1,4 +1,4 @@
-package com.y.ybackend;
+package com.y.back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,5 +9,4 @@ class YBackEndApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
