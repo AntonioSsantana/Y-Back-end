5ec545dffd035f8c566984268c47b020976f12c5
Antonio Santana <antonionetosg6@gmail.com>
2024-04-03 12:54:58 -0300

    dev: development branch

--- src/main/java/com/y/back/YBackEndApplication.java
+++ src/main/java/com/y/back/YBackEndApplication.java
@@ -3,6 +3,9 @@ package com.y.back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Development Branch.
+ */
 @SpringBootApplication
 public class YBackEndApplication {
 
